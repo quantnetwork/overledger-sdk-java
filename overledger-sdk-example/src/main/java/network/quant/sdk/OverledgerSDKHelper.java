275b9f13ebce686139fdd720a5a828f67af032d8
Rui Wang <rui.wang@quant.network>
2019-02-26 11:02:01 +0000

    remove un-used prints

--- overledger-sdk-example/src/main/java/network/quant/sdk/OverledgerSDKHelper.java
+++ overledger-sdk-example/src/main/java/network/quant/sdk/OverledgerSDKHelper.java
@@ -267,12 +267,8 @@ public class OverledgerSDKHelper {
                 );
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
             OverledgerTransaction overledgerTransaction = this.overledgerSDK.writeTransaction(transaction);
             this.applicationDataHandler.onPurchaseSuccess(overledgerTransaction);
-            System.out.println(objectMapper.writeValueAsString(transaction));
-            System.out.println("##########");
-            System.out.println(objectMapper.writeValueAsString(overledgerTransaction));
         } catch (Exception e) {
             if (e instanceof ClientResponseException) {
                 ClientResponseException clientResponseException = (ClientResponseException) e;
