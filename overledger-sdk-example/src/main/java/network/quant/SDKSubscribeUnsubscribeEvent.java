5d05ef4d5d2851ff319e1f91b37fcf337c3f6c16
mahsa <mahsa.bayat@quant.network>
2020-10-16 15:29:18 +0100

    11551 subscribe event timeout changes

--- overledger-sdk-example/src/main/java/network/quant/SDKSubscribeUnsubscribeEvent.java
+++ overledger-sdk-example/src/main/java/network/quant/SDKSubscribeUnsubscribeEvent.java
@@ -22,19 +22,6 @@ public class SDKSubscribeUnsubscribeEvent {
         String smartContractAddress = "0x19Bc592A0E1BAb3AFFB1A8746D8454743EE6E838";
 
         //the order of contract arguments are important
-        List<ContractArgument> inputValues = new ArrayList<ContractArgument>() {
-            {
-                add(ContractArgument.builder()
-                        .type(ContractInputTypeOptions.ADDRESS)
-                        .value("0x650A87cfB9165C9F4Ccc7B971D971f50f753e761")
-                        .build());
-                add(ContractArgument.builder()
-                        .type(ContractInputTypeOptions.UINT)
-                        .selectedIntegerLength(EthereumUintIntOptions.B256)
-                        .build());
-            }
-        };
-
         EventSubscribeRequestDetails eventSubscribeRequestDetails1 = EventSubscribeRequestDetails.builder()
                 .contractAddress(smartContractAddress)
                 .eventName("Approval")
@@ -46,50 +33,8 @@ public class SDKSubscribeUnsubscribeEvent {
                 .callBackURL("http://localhost:8086/webhook/dummyDisplay")
                 .build();
 
-        EventSubscribeRequestDetails eventSubscribeRequestDetails2 = EventSubscribeRequestDetails.builder()
-                .contractAddress(smartContractAddress)
-                .eventName("Transfer")
-                .eventParams(Arrays.asList(
-                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS).build(),
-                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS).build(),
-                        ContractArgument.builder().type(ContractInputTypeOptions.UINT).selectedIntegerLength(EthereumUintIntOptions.B256).build()
-                ))
-                .callBackURL("http://localhost:8086/webhook/dummyDisplay")
-                .build();
-
-        EventSubscribeRequestDetails eventSubscribeRequestDetails3 = EventSubscribeRequestDetails.builder()
-                .contractAddress("0x8781d54e454377451D9C6928538Db544Caa65CDf")
-                .eventName("arraysDynamic")
-                .eventParams(Arrays.asList(
-                        ContractArgument.builder().type(ContractInputTypeOptions.UINT_ARRAY).selectedIntegerLength(EthereumUintIntOptions.B256).build(),
-                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS_ARRAY).build(),
-                        ContractArgument.builder().type(ContractInputTypeOptions.BOOLEAN_ARRAY).build()
-                ))
-                .callBackURL("http://localhost:8086/webhook/dummyDisplay")
-                .build();
-
-        EventSubscribeRequestDetails eventSubscribeRequestDetails4 = EventSubscribeRequestDetails.builder()
-                .contractAddress("0x8781d54e454377451D9C6928538Db544Caa65CDf")
-                .eventName("arraysFixed")
-                .eventParams(Arrays.asList(
-                        ContractArgument.builder().type(ContractInputTypeOptions.UINT_ARRAY)
-                                .selectedIntegerLength(EthereumUintIntOptions.B256)
-                                .selectedArrayLength(3l).build(),
-                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS_ARRAY).selectedArrayLength(2l).build(),
-                        ContractArgument.builder().type(ContractInputTypeOptions.BOOLEAN_ARRAY).selectedArrayLength(4l).build()
-
-                ))
-                .callBackURL("http://localhost:8086/webhook/dummyDisplay")
-                .build();
-
-
         EventSubscribeRequest eventSubscribeRequest = EventSubscribeRequest.builder()
-                .requestDetailsList(Arrays.asList(
-                        eventSubscribeRequestDetails1,
-                        eventSubscribeRequestDetails2,
-                        eventSubscribeRequestDetails3,
-                        eventSubscribeRequestDetails4
-                ))
+                .requestDetailsList(Arrays.asList(eventSubscribeRequestDetails1))
                 .build();
 
         List<EventSubscribeResponse> subscribeEventResponse = sdk.eventSubscribe(eventSubscribeRequest);
--- overledger-sdk-example/src/test/java/io/crypto/overledger/OverleddgerSDKTest.java
+++ overledger-sdk-example/src/test/java/io/crypto/overledger/OverleddgerSDKTest.java
@@ -7,18 +7,37 @@ import network.quant.api.OverledgerSDK;
 import network.quant.bitcoin.BitcoinAccount;
 import network.quant.essential.DefaultOverledgerSDK;
 import network.quant.ethereum.EthereumAccount;
+import network.quant.ethereum.experimental.dto.*;
 import network.quant.ripple.RippleAccount;
+import network.quant.util.EventSubscribeResponse;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.xml.bind.DatatypeConverter;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 
+
+@RunWith(MockitoJUnitRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OverleddgerSDKTest {
 
+    @Mock
     private OverledgerSDK overledgerSDK;
     private Account bitcoinAccount;
     private Account ethereumAccount;
     private Account rippleAccount;
+    private EventSubscribeRequest fixedArrayEventSubscribeRequest;
+    private EventSubscribeRequest transferEventSubscribeRequest;
+    private EventSubscribeRequest dynamicArrayEventSubscribeRequest;
 
     @Before
     public void setup() {
@@ -32,6 +51,100 @@ public class OverleddgerSDKTest {
         this.overledgerSDK.addAccount(DLT.bitcoin.name(), this.bitcoinAccount);
         this.overledgerSDK.addAccount(DLT.ethereum.name(), this.ethereumAccount);
         this.overledgerSDK.addAccount(DLT.ripple.name(), this.rippleAccount);
+
+
+        fixedArrayEventSubscribeRequest = EventSubscribeRequest.builder()
+                .dlt("ethereum")
+                .requestDetailsList(Arrays.asList(
+                        EventSubscribeRequestDetails.builder()
+                                .contractAddress("0x8781d54e454377451D9C6928538Db544Caa65CDf")
+                                .eventName("arraysFixed")
+                                .eventParams(Arrays.asList(
+                                        ContractArgument.builder().type(ContractInputTypeOptions.UINT_ARRAY)
+                                                .selectedIntegerLength(EthereumUintIntOptions.B256)
+                                                .selectedArrayLength(3l).build(),
+                                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS_ARRAY).selectedArrayLength(2l).build(),
+                                        ContractArgument.builder().type(ContractInputTypeOptions.BOOLEAN_ARRAY).selectedArrayLength(4l).build()
+
+                                ))
+                                .callBackURL("http://localhost:8086/webhook/dummyDisplay")
+                                .build())
+                ).build();
+
+        transferEventSubscribeRequest = EventSubscribeRequest.builder()
+                .dlt("ethereum")
+                .requestDetailsList(Arrays.asList(
+                        EventSubscribeRequestDetails.builder()
+                                .contractAddress("0x19Bc592A0E1BAb3AFFB1A8746D8454743EE6E838")
+                                .eventName("Transfer")
+                                .eventParams(Arrays.asList(
+                                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS).build(),
+                                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS).build(),
+                                        ContractArgument.builder().type(ContractInputTypeOptions.UINT).selectedIntegerLength(EthereumUintIntOptions.B256).build()
+                                ))
+                                .callBackURL("http://localhost:8086/webhook/dummyDisplay")
+                                .build())
+                ).build();
+
+        dynamicArrayEventSubscribeRequest = EventSubscribeRequest.builder()
+                .dlt("ethereum")
+                .requestDetailsList(Arrays.asList(
+                        EventSubscribeRequestDetails.builder()
+                                .contractAddress("0x8781d54e454377451D9C6928538Db544Caa65CDf")
+                                .eventName("arraysDynamic")
+                                .eventParams(Arrays.asList(
+                                        ContractArgument.builder().type(ContractInputTypeOptions.UINT_ARRAY).selectedIntegerLength(EthereumUintIntOptions.B256).build(),
+                                        ContractArgument.builder().type(ContractInputTypeOptions.ADDRESS_ARRAY).build(),
+                                        ContractArgument.builder().type(ContractInputTypeOptions.BOOLEAN_ARRAY).build()
+                                ))
+                                .callBackURL("http://localhost:8086/webhook/dummyDisplay")
+                                .build())
+                ).build();
+    }
+
+    @Test
+    public void test001EthSubscribeEvent_FixedArrayEvent_shouldSuccess() throws Exception {
+        EventSubscribeResponse expectedEventSubscribeResponse = new EventSubscribeResponse("Subscribed successfully"
+                + ", contractAddress: " + fixedArrayEventSubscribeRequest.getRequestDetailsList().get(0).getContractAddress()
+                + ", EventName: " + fixedArrayEventSubscribeRequest.getRequestDetailsList().get(0).getEventName()
+                + ", callBackURL: " + fixedArrayEventSubscribeRequest.getRequestDetailsList().get(0).getCallBackURL());
+
+        Mockito.when(this.overledgerSDK.eventSubscribe(fixedArrayEventSubscribeRequest)).
+                thenReturn(Arrays.asList(expectedEventSubscribeResponse));
+
+        List<EventSubscribeResponse> actualResponse = this.overledgerSDK.eventSubscribe(fixedArrayEventSubscribeRequest);
+        Assert.assertNotNull(actualResponse);
+        Assert.assertEquals(expectedEventSubscribeResponse.getPayload(), actualResponse.get(0).getPayload());
+    }
+
+    @Test
+    public void test002EthSubscribeEvent_transferEvent_shouldSuccess() throws Exception {
+        EventSubscribeResponse expectedEventSubscribeResponse = new EventSubscribeResponse("Subscribed successfully"
+                + ", contractAddress: " + transferEventSubscribeRequest.getRequestDetailsList().get(0).getContractAddress()
+                + ", EventName: " + transferEventSubscribeRequest.getRequestDetailsList().get(0).getEventName()
+                + ", callBackURL: " + transferEventSubscribeRequest.getRequestDetailsList().get(0).getCallBackURL());
+
+        Mockito.when(this.overledgerSDK.eventSubscribe(transferEventSubscribeRequest)).
+                thenReturn(Arrays.asList(expectedEventSubscribeResponse));
+
+        List<EventSubscribeResponse> actualResponse = this.overledgerSDK.eventSubscribe(transferEventSubscribeRequest);
+        Assert.assertNotNull(actualResponse);
+        Assert.assertEquals(expectedEventSubscribeResponse.getPayload(), actualResponse.get(0).getPayload());
+    }
+
+    @Test
+    public void test003EthSubscribeEvent_DynamicArrayEvent_shouldSuccess() throws Exception {
+        EventSubscribeResponse expectedEventSubscribeResponse = new EventSubscribeResponse("Subscribed successfully"
+                + ", contractAddress: " + dynamicArrayEventSubscribeRequest.getRequestDetailsList().get(0).getContractAddress()
+                + ", EventName: " + dynamicArrayEventSubscribeRequest.getRequestDetailsList().get(0).getEventName()
+                + ", callBackURL: " + dynamicArrayEventSubscribeRequest.getRequestDetailsList().get(0).getCallBackURL());
+
+        Mockito.when(this.overledgerSDK.eventSubscribe(dynamicArrayEventSubscribeRequest)).
+                thenReturn(Arrays.asList(expectedEventSubscribeResponse));
+
+        List<EventSubscribeResponse> actualResponse = this.overledgerSDK.eventSubscribe(dynamicArrayEventSubscribeRequest);
+        Assert.assertNotNull(actualResponse);
+        Assert.assertEquals(expectedEventSubscribeResponse.getPayload(), actualResponse.get(0).getPayload());
     }
 
 }
