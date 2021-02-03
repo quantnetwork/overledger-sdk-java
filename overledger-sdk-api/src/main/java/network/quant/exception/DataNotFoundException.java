20d2de877dc7d15619cbd6bf4152d0975d8a0c90
kashif-quant <kashif.hayat@quant.network>
2021-02-03 18:20:16 +0000

    Update class documentation

--- overledger-sdk-api/src/main/java/network/quant/exception/DataNotFoundException.java
+++ overledger-sdk-api/src/main/java/network/quant/exception/DataNotFoundException.java
@@ -1,7 +1,7 @@
 package network.quant.exception;
 
 /**
- *  This exception is thrown when no Quant message can be found in transaction.
+ *  This exception is thrown when no message message can be found in transaction.
  *
  *  @author     Quant network
  *  @see        <a href="http://quant.network">http://quant.network</a>
