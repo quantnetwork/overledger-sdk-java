d74236aea7112c987388b2f80205559b6d6c19c5
richardbahari <64963318+richardbahari@users.noreply.github.com>
2021-02-18 08:56:34 +0000

    minor log change

--- overledger-sdk-example/src/main/java/network/quant/SDKOauthSample.java
+++ overledger-sdk-example/src/main/java/network/quant/SDKOauthSample.java
@@ -9,20 +9,15 @@ import network.quant.util.FeeEstimationResponse;
  */
 final class SDKOauthSample {
 
-
     public static void main(String[] args) {
-
         try {
             OverledgerSDK sdk = DefaultOverledgerSDK.newInstance();
 
             System.out.println("Result after refreshing: " + sdk.refreshAccessToken());
 
         } catch (Exception e) {
-            System.out.println("Some error happened in the demo, make sure you have the config file in the src/main/resources set and the oauth2 values set in the context.properties");
+            System.out.println("Some error occurred in the demo, make sure you have the config file in the src/main/resources set and the oauth2 values set in the context.properties");
             e.printStackTrace();
         }
-
-
     }
-
 }
